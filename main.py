40a54719c5afda03d3ab900284399747ac777d9a
Kami Salibayeva <61473793+KSalibay@users.noreply.github.com>
2023-04-16 19:43:57 -0400

    First attempt at making epoching work (main.py)

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
-def epoch(param_meg,param_eeg,param_eog,param_ecg,param_emg,param_stim, plot_cond, pick, event_id, raw, events, tmin, tmax):
+def epoch(param_meg,param_eeg,param_eog,param_ecg,param_emg,param_stim, plot_cond, pick, event_id, raw, events, tmin, tmax, metadata):
     raw.pick_types(meg=param_meg,eeg=param_eeg,eog=param_eog,ecg=param_ecg,emg=param_emg, stim=param_stim).load_data()
 
     report = mne.Report(title='Report')
@@ -40,20 +40,14 @@ def epoch(param_meg,param_eeg,param_eog,param_ecg,param_emg,param_stim, plot_con
     sfreq = raw.info['sfreq']
     #report.add_events(events=events, title='Events', sfreq=sfreq)
 
-    metadata, _, _ = mne.epochs.make_metadata(
-        events=events,
-        event_id=event_id,
-        tmin=tmin,
-        tmax=tmax,
-        sfreq=raw.info['sfreq']
-    )
-  
-    epochs = mne.Epochs(
-        raw=raw, events=events, event_id=event_id, metadata=metadata
-    )
-
+    epochs = mne.Epochs(raw=raw, events=events, event_id=event_id, metadata=metadata, tmin=tmin, tmax=tmax)
+    epochs = epochs['response_correct']
     report.add_epochs(epochs=epochs, title='Epochs from "epochs"')
 
+    correct_response_count = metadata['response_correct'].sum()
+    report.add_html('<dev>'+print(f'Correct responses: {correct_response_count}\n'
+          f'Incorrect responses: {len(metadata) - correct_response_count}')+'</dev>')
+    
     # == SAVE REPORT ==
     report.save('out_dir_report/report.html', overwrite=True)
     sys.stdout.write('test')
@@ -71,6 +65,7 @@ def epoch(param_meg,param_eeg,param_eog,param_ecg,param_emg,param_stim, plot_con
     plt.figure(1)
     fig_ep = epochs[plot_cond].plot_image(picks=[pick])
     fig_ep[0].savefig(os.path.join('out_figs','epoimg.png'))
+
 
 def main():
     # Load inputs from config.json
@@ -111,13 +106,38 @@ def main():
     event_id = dict((x.strip(), int(y.strip()))
                      for x, y in (element.split('-')
                                   for element in  event_id_condition.split(', ')))
+    
+    metadata_tmin, metadata_tmax = config['metadata_tmin'], config['metadata_tmax']
 
-    id_list = list(event_id.values())
+    row_events = [k for k in event_id.keys() if k[0:7] == 'stimulus']
+
+    keep_last = ['stimulus', 'response']
+
+    metadata, events, event_id = mne.epochs.make_metadata(
+        events=events, event_id=event_id, 
+        tmin=metadata_tmin, tmax=metadata_tmax, sfreq=raw.info['sfreq'],
+        row_events=row_events,
+        keep_last=keep_last)
+    
+    target_left = [stim for stim in row_events if stim[-4:-1] == 'left']
+    target_right = [stim for stim in row_events if stim[-5:-1] == 'right']
+
+    metadata.loc[metadata['last_stimulus'].isin(target_left),
+              'stimulus_side'] = 'left'
+    metadata.loc[metadata['last_stimulus'].isin(target_right),
+              'stimulus_side'] = 'right'
+
+    metadata['response_correct'] = False
+    metadata.loc[metadata['stimulus_side'] == metadata['last_response'],
+             'response_correct'] = True
+    
+    id_list = [v for k, v in event_id.items() if k[0:3] != 'stim']
 
     events = mne.pick_events(events, include=id_list)
 
-  
-    epochs = epoch(config['pick_meg'],config['pick_eeg'],config['pick_eog'], config['pick_ecg'],config['pick_emg'],config['param_stim'], config['plot_cond'], config['pick'], event_id, raw, events, tmin=tmin, tmax=tmax)
+    epochs = epoch(config['pick_meg'],config['pick_eeg'],config['pick_eog'], 
+                   config['pick_ecg'],config['pick_emg'],config['param_stim'], config['plot_cond'], 
+                   config['pick'], event_id, raw, events, tmin=tmin, tmax=tmax, metadata=metadata)
 
 if __name__ == '__main__':
     main()
