dc62ef51844d7ad82c81164ebde648d97b5d1e0e
Kami Salibayeva <61473793+KSalibay@users.noreply.github.com>
2023-04-29 22:13:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ with open('config.json') as config_json:
 
 # Read the meg file
 data_file = config.pop('fif')
-report = mne.Report(title='Report')
+
 # Read the event time
 tmin = config.pop('tmin')
 tmax = config.pop('tmax')
@@ -38,69 +38,52 @@ tmax = config.pop('tmax')
 raw = mne.io.read_raw_fif(data_file, verbose=False)
 #mask = 4096 + 256  # mask for excluding high order bits
 
-#if 'events' in config.keys():
-#    events_file = config.pop('events')
-#    if op.exists(events_file):
-#        events = mne.read_events(events_file)
-#    else:
-#        events = mne.find_events(raw, stim_channel=config['stim_channel'],
-#                         consecutive='increasing', mask=mask,
-#                         mask_type='not_and', min_duration=0.003)
-#else:
-#    events = mne.find_events(raw, stim_channel=config['stim_channel'],
-#                         consecutive='increasing', mask=mask,
-#                         mask_type='not_and', min_duration=0.003)
+if config['events'] is not None:
+   events_file = config.pop('events')
+   if op.exists(events_file):
+       events = mne.read_events(events_file)
+   else:
+       events = mne.find_events(raw, stim_channel=config['stim_channel'])
+else:
+   events = mne.find_events(raw, stim_channel=config['stim_channel'])
 
-
-events = mne.find_events(raw, stim_channel=config['stim_channel'])
 event_id_condition= config['event_id_condition_mapping']
 
 #Convert String to Dictionary using strip() and split() methods
-event_id0 = dict((x.strip(), int(y.strip()))
+event_id = dict((x.strip(), int(y.strip()))
                  for x, y in (element.split('-')
                               for element in event_id_condition.split(',')))
-if config['assess_correctness'] == True:
-    metadata_tmin, metadata_tmax = config['metadata_tmin'], config['metadata_tmax']
-    
-    row_events = [k for k in event_id0.keys() if 'stimulus' in k]
-    
-    keep_last = ['stimulus', 'response']
-    
-    metadata, events, event_id = mne.epochs.make_metadata(
-        events=events, event_id=event_id0, 
-        tmin=metadata_tmin, tmax=metadata_tmax, sfreq=raw.info['sfreq'],
-        row_events=row_events,
-        keep_last=keep_last)
-    
-    responses = [k for k in event_id0.keys() if 'response' in k]
-    resp1 = responses[0].split('/')[1]
-    resp2 = responses[1].split('/')[1]
-    
-    target_left = [stim.partition('/')[2] for stim in row_events if resp1 in stim.split('/')[2]]
-    target_right = [stim.partition('/')[2] for stim in row_events if resp2 in stim.split('/')[2]]
-    
-    metadata.loc[metadata['last_stimulus'].isin(target_left),
-              'stimulus_side'] = resp1
-    metadata.loc[metadata['last_stimulus'].isin(target_right),
-              'stimulus_side'] = resp2
-    
-    metadata['response_correct'] = False
-    metadata.loc[metadata['stimulus_side'] == metadata['last_response'],
-             'response_correct'] = True
-    
-    id_list = [v for k, v in event_id.items() if k[0:3] != 'stim']
-    correct_response_count = metadata['response_correct'].sum()
-    incorrect_response_count = len(metadata) - correct_response_count
 
-    report.add_html(title='Counts of correct responses',
-                    html='<dev>'+'Correct responses: '+str(correct_response_count)+
-                    '<br> Incorrect responses: '+str(incorrect_response_count)+'</dev>')
-else:
-    metadata, events, event_id = mne.epochs.make_metadata(
-        events=events, event_id=event_id, 
-        tmin=tmin, tmax=tmax, sfreq=raw.info['sfreq'])
+
+metadata_tmin, metadata_tmax = config['metadata_tmin'], config['metadata_tmax']
+
+row_events = [k for k in event_id.keys() if 'stimulus' in k]
+
+keep_last = ['stimulus', 'response']
+
+metadata, events, event_id = mne.epochs.make_metadata(
+    events=events, event_id=event_id, 
+    tmin=metadata_tmin, tmax=metadata_tmax, sfreq=raw.info['sfreq'],
+    row_events=row_events,
+    keep_last=keep_last)
+
+target_left = [stim[9:] for stim in row_events if stim[-4:] == 'left']
+target_right = [stim[9:] for stim in row_events if stim[-5:] == 'right']
+
+metadata.loc[metadata['last_stimulus'].isin(target_left),
+          'stimulus_side'] = 'left'
+metadata.loc[metadata['last_stimulus'].isin(target_right),
+          'stimulus_side'] = 'right'
+
+metadata['response_correct'] = False
+metadata.loc[metadata['stimulus_side'] == metadata['last_response'],
+         'response_correct'] = True
+
+id_list = [v for k, v in event_id.items() if k[0:3] != 'stim']
 
 events = mne.pick_events(events, include=id_list)
+
+report = mne.Report(title='Report')
 
 #raw
 #report.add_raw(raw=raw, title='Raw', psd=False)  # omit PSD plot
@@ -110,14 +93,17 @@ sfreq = raw.info['sfreq']
 #report.add_events(events=events, title='Events', sfreq=sfreq)
 
 epochs = mne.Epochs(raw=raw, events=events, event_id=event_id, metadata=metadata, tmin=tmin, tmax=tmax)
-if config['use_correct']==True:
-    epochs = epochs['response_correct']
-
+epochs = epochs['response_correct']
 report.add_epochs(epochs=epochs, title='Epochs from "epochs"')
+
+correct_response_count = metadata['response_correct'].sum()
+incorrect_response_count = len(metadata) - correct_response_count
+
+report.add_html(title='Counts of correct responses',html='<dev>'+'Correct responses: '+str(correct_response_count)+
+       'Incorrect responses: '+str(incorrect_response_count)+'</dev>')
  
  # == SAVE REPORT ==
-report.save('report.html',overwrite=True)
-sys.stdout.write('test')
+report.save(os.path.join('out_dir','report.html'))
 
  # == SAVE FILE ==
-epochs.save(os.path.join('out_dir', 'epo.fif'), overwrite=True)
+epochs.save(os.path.join('out_dir', 'meg-epo.fif'), overwrite=True)
